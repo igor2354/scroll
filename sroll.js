3027acac4a11b099dfb00eed43656010b888e6ac
igor2354 <66739722+igor2354@users.noreply.github.com>
2020-10-20 17:34:11 +0300

    Create sroll.js

---
+++ sroll.js
@@ -0,0 +1,28 @@
+  const animItems = $(".anim-items");
+
+  if (animItems.length > 0) {
+    $(window).on("scroll", animOnScroll);
+    function animOnScroll() {
+      $.each(animItems, function (index, val) {
+        const animItem = animItems.eq(index);
+        const animItemHeight = animItem.innerHeight();
+        const animItemOffset = animItem.offset().top;
+        const animStart = 10; // начало анимации при достижении скролом 1/10 части элемента
+
+        let animItemPoint = $(window).height() - animItemHeight / animStart;
+
+        if (animItemHeight > $(window).height()) {
+          animItemPoint = $(window).height() - $(window).height() / animStart;
+        }
+
+        if ($(window).scrollTop() > animItemOffset - animItemPoint && $(window).scrollTop() < animItemOffset + animItemHeight) {
+          animItem.addClass("animate");
+        } else {
+          if (!animItem.hasClass("anim-no-scrollTop")) {
+            animItem.removeClass("animate");
+          }
+        }
+      });
+    }
+    setTimeout(animOnScroll, 0);
+  }
